cfccf9b7f82b176fd08acf873706e226afb4a21f
ElTinmar <martin.privat@hotmail.fr>
2025-06-18 13:38:53 +0200

    first impl

--- daq_tools/national_instrument .py
+++ daq_tools/national_instrument .py
@@ -12,30 +12,50 @@ logger = logging.getLogger(__name__)
 
 class NI_SoftTiming(SoftwareTimingDAQ):
 
-    def __init__(self, device_name: str) -> None:
+    def __init__(self, device_index: int) -> None:
 
         super().__init__()
 
         system = nidaqmx.system.System.local()
-
+        self.device = system.devices[device_index] 
         self._closed = False
-        logger.info(f"Connected to NI : ")
+        logger.info(f"Connected to NI: {self.device.name}")
         self.reset_state()
 
     def analog_write(self, channel: int, val: float) -> None:
-        pass
+        ao_channel = self.device.ao_physical_chans[channel]
+        with nidaqmx.Task() as task:
+            task.ao_channels.add_ao_voltage_chan(ao_channel.name) 
+            task.write(val)
 
     def analog_read(self, channel: int) -> float:
-        pass
+        ai_channel = self.device.ai_physical_chans[channel]
+        with nidaqmx.Task() as task:
+            task.ai_channels.add_ai_voltage_chan(ai_channel.name) 
+            val = task.read()
+        return val
 
     def digital_write(self, channel: int, val: bool) -> None:
-        pass
+        do_channel = self.device.do_lines[channel]
+        with nidaqmx.Task() as task:
+            task.do_channels.add_do_chan(do_channel.name)
+            task.write(val)
 
     def digital_read(self, channel: int) -> bool:
-        pass
+        di_channel = self.device.di_lines[channel]
+        with nidaqmx.Task() as task:
+            task.di_channels.add_di_chan(di_channel.name)
+            val = task.read()
+        return val
 
     def pwm_write(self, channel: int, duty_cycle: float) -> None:
-        pass
+        pwm_channel = self.device.co_physical_chans[channel]
+        with nidaqmx.Task() as task: 
+            task.co_channels.add_co_pulse_chan_freq(
+                pwm_channel.name, 
+                duty_cycle = duty_cycle, 
+                freq = 1000,
+            )
 
     def close(self) -> None:
         if self._closed:
@@ -54,62 +74,17 @@ class NI_SoftTiming(SoftwareTimingDAQ):
     @classmethod
     def list_boards(cls) -> List[BoardInfo]:
         system = nidaqmx.system.System.local()
-        for dev in system.devices:
-            print(dev.name)
+        boards = []
+        for idx, dev in enumerate(system.devices):
+            boards.append(BoardInfo(id=idx, name=dev.name))
 
-
-# analog read  two channels
-with nidaqmx.Task() as task:
-    task.ai_channels.add_ai_voltage_chan("NIPCI-6110/ai0", min_val=-10.0, max_val=10.0)
-    task.ai_channels.add_ai_voltage_chan("NIPCI-6110/ai1", min_val=-10.0, max_val=10.0)
-    data = task.read(number_of_samples_per_channel=1000)
-
-# analog read multiple points / hardware timings
-with nidaqmx.Task() as task:
-    task.ai_channels.add_ai_voltage_chan("NIPCI-6110/ai0", min_val=-10.0, max_val=10.0)
-    task.timing.cfg_samp_clk_timing(1000, sample_mode=AcquisitionType.FINITE, samps_per_chan=1000)
-    data = task.read(READ_ALL_AVAILABLE)
-     
-# analog write
-with nidaqmx.Task() as task:
-    task.ao_channels.add_ao_voltage_chan("NIPCI-6110/ao0")
-    task.write([1.1, 2.2, 3.3, 4.4, 5.5], auto_start=True)
-
-# digital read
-with nidaqmx.Task() as task:
-    task.di_channels.add_di_chan("NIPCI-6110/port0/line0")
-    data = task.read()
-
-# digital write
-with nidaqmx.Task() as task:
-    task.do_channels.add_do_chan("NIPCI-6110/port0/line0")
-    task.write(True)
-
-# PWM
-with nidaqmx.Task() as task:
-    channel = task.co_channels.add_co_pulse_chan_freq("NIPCI-6110/ctr0", duty_cycle=0.5, freq=1000)
-    channel.co_pulse_term = "/NIPCI-6110/PFI9"
-    task.start()
-    task.wait_until_done(timeout=5)
-    task.stop()
+        logger.debug(f"Found {len(boards)} supported U3 board(s).")
+        return boards
 
 # stream
-with nidaqmx.Task() as task:
-    task.ai_channels.add_ai_voltage_chan("NIPCI-6110/ai0", min_val=-10.0, max_val=10.0)
-    stream = AnalogSingleChannelReader(task.in_stream)
-    data = np.zeros((1000,), dtype=np.float64)
-    stream.read_many_sample(data, number_of_samples_per_channel=1000)
+#with nidaqmx.Task() as task:
+#    task.ai_channels.add_ai_voltage_chan("NIPCI-6110/ai0", min_val=-10.0, max_val=10.0)
+#    stream = AnalogSingleChannelReader(task.in_stream)
+#    data = np.zeros((1000,), dtype=np.float64)
+#    stream.read_many_sample(data, number_of_samples_per_channel=1000)
     
-
-
-import pprint
-pp = pprint.PrettyPrinter(indent=4)
-system = nidaqmx.system.System.local()
-for dev in system.devices:
-    pp.pprint(dev.name)
-    pp.pprint(dev.ai_physical_chans[:])
-    pp.pprint(dev.ao_physical_chans[:])
-    pp.pprint(dev.di_lines[:])
-    pp.pprint(dev.do_lines[:])
-    pp.pprint(dev.ci_physical_chans[:])
-    pp.pprint(dev.co_physical_chans[:])
