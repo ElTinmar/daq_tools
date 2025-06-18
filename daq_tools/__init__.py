8629f219fe2339e5501de367abb5347b8dcde0d0
ElTinmar <martin.privat@hotmail.fr>
2025-06-18 18:31:52 +0200

    s

--- daq_tools/__init__.py
+++ daq_tools/__init__.py
@@ -1,4 +1,4 @@
 from .core import SoftwareTimingDAQ
 from .arduino import Arduino_SoftTiming
 from .labjack import LabJackU3_SoftTiming
-from .national_instrument import NI_SoftTiming
+from .national_instruments import NI_SoftTiming
