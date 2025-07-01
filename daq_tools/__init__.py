bd301fde53189923fd52fb34e287adfa17a7bbd2
ElTinmar <martin.privat@hotmail.fr>
2025-07-01 13:22:27 +0200

    add exception and boardinfo

--- daq_tools/__init__.py
+++ daq_tools/__init__.py
@@ -1,4 +1,4 @@
-from .core import SoftwareTimingDAQ
+from .core import SoftwareTimingDAQ, BoardInfo, DAQReadError
 from .arduino import Arduino_SoftTiming
 from .labjack import LabJackU3_SoftTiming
 from .national_instruments import NI_SoftTiming
