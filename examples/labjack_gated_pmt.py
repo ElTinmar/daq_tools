7a46ec61e151a7155e63db1d195285c73b144f6b
ElTinmar <martin.privat@hotmail.fr>
2025-07-01 10:42:47 +0200

    added logging

--- examples/labjack_gated_pmt.py
+++ examples/labjack_gated_pmt.py
@@ -1,19 +1,27 @@
 from daq_tools import LabJackU3_SoftTiming
 import time
+import logging
 
 DURATION_SEC = 10
 FREQUENCY_HZ = 20
 
-with LabJackU3_SoftTiming.auto_connect() as daq:
+if __name__ == '__main__':
 
-    for j in range(DURATION_SEC):
-        for i in range(FREQUENCY_HZ):
-            daq.digital_write(0, True)
-            time.sleep(0.45*1/FREQUENCY_HZ)
-            daq.digital_write(2, True)
-            time.sleep(0.05 * 1/FREQUENCY_HZ)
-            daq.digital_write(0, False)
-            time.sleep(0.05 * 1/FREQUENCY_HZ)
-            daq.digital_write(2, False)
-            time.sleep(0.45 * 1/FREQUENCY_HZ)
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    )
+
+    with LabJackU3_SoftTiming.auto_connect() as daq:
+
+        for j in range(DURATION_SEC):
+            for i in range(FREQUENCY_HZ):
+                daq.digital_write(0, True)
+                time.sleep(0.45*1/FREQUENCY_HZ)
+                daq.digital_write(2, True)
+                time.sleep(0.05 * 1/FREQUENCY_HZ)
+                daq.digital_write(0, False)
+                time.sleep(0.05 * 1/FREQUENCY_HZ)
+                daq.digital_write(2, False)
+                time.sleep(0.45 * 1/FREQUENCY_HZ)
 
--- examples/non_blocking_pwm.py
+++ examples/non_blocking_pwm.py
@@ -1,6 +1,14 @@
 from daq_tools import Arduino_SoftTiming
+import logging
 
-daq = Arduino_SoftTiming.auto_connect()
-daq.pwm_pulse(3, 5, 0.05, blocking=False)
-daq.pwm_pulse(9, 5, 0.5, blocking=True)
-daq.close()
+if __name__ == '__main__':
+
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    )
+
+    daq = Arduino_SoftTiming.auto_connect()
+    daq.pwm_pulse(3, 5, 0.05, blocking=False)
+    daq.pwm_pulse(9, 5, 0.5, blocking=True)
+    daq.close()
