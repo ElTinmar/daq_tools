2a3069f2fd97e1f557ab313e86004a4c65b490a1
ElTinmar <martin.privat@hotmail.fr>
2025-07-01 13:25:24 +0200

    bumpo version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='daq_tools',
     python_requires='>=3.8',
     author='Martin Privat',
-    version='0.1.0',
+    version='0.1.1',
     packages=['daq_tools'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     description='control software and hardware timed DAQ',
