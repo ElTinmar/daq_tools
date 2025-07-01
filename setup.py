c825c6fe9e44c0f655530a74880814cefc5308c9
ElTinmar <martin.privat@hotmail.fr>
2025-07-01 10:59:48 +0200

    fix

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 
 setup(
-    name='ZebVR',
+    name='daq_tools',
     python_requires='>=3.8',
     author='Martin Privat',
     version='0.1.0',
-    packages=['ZebVR'],
+    packages=['daq_tools'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    description='Open and closed-loop virtual reality',
+    description='control software and hardware timed DAQ',
     long_description=open('README.md').read(),
     install_requires=[
         "labjackpython",
