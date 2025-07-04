59d0b08b11648a1d01909f30f2674e9f62b09395
ElTinmar <martin.privat@hotmail.fr>
2025-07-04 10:23:24 +0200

    change dict name

--- daq_tools/__init__.py
+++ daq_tools/__init__.py
@@ -2,8 +2,9 @@ from .core import SoftwareTimingDAQ, BoardInfo, DAQReadError, BoardType
 from .arduino import Arduino_SoftTiming
 from .labjack import LabJackU3_SoftTiming
 from .national_instruments import NI_SoftTiming
+from typing import Dict
 
-board_type_constructors = {
+DAQ_CONSTRUCTORS: Dict[BoardType, SoftwareTimingDAQ] = {
     BoardType.ARDUINO: Arduino_SoftTiming,
     BoardType.LABJACK: LabJackU3_SoftTiming,
     BoardType.NATIONAL_INSTRUMENTS: NI_SoftTiming
