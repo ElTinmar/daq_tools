6aa7a8791884028a68eda0a62ecd03fa75e43bf9
ElTinmar <martin.privat@hotmail.fr>
2025-07-05 11:45:51 +0200

    fix type hints

--- daq_tools/__init__.py
+++ daq_tools/__init__.py
@@ -2,9 +2,9 @@ from .core import SoftwareTimingDAQ, BoardInfo, DAQReadError, BoardType
 from .arduino import Arduino_SoftTiming
 from .labjack import LabJackU3_SoftTiming
 from .national_instruments import NI_SoftTiming
-from typing import Dict
+from typing import Dict, Type
 
-DAQ_CONSTRUCTORS: Dict[BoardType, SoftwareTimingDAQ] = {
+DAQ_CONSTRUCTORS: Dict[BoardType, Type[SoftwareTimingDAQ]] = {
     BoardType.ARDUINO: Arduino_SoftTiming,
     BoardType.LABJACK: LabJackU3_SoftTiming,
     BoardType.NATIONAL_INSTRUMENTS: NI_SoftTiming
