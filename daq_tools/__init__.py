a4b6193a2c6ccb378c958b6ab03f0eeef0afb321
ElTinmar <martin.privat@hotmail.fr>
2025-07-06 23:14:42 +0200

    update

--- daq_tools/__init__.py
+++ daq_tools/__init__.py
@@ -1,11 +1,24 @@
-from .core import SoftwareTimingDAQ, BoardInfo, DAQReadError, BoardType
-from .arduino import Arduino_SoftTiming
-from .labjack import LabJackU3_SoftTiming
-from .national_instruments import NI_SoftTiming
 from typing import Dict, Type
+from .core import SoftwareTimingDAQ, BoardInfo, DAQReadError, BoardType
 
-DAQ_CONSTRUCTORS: Dict[BoardType, Type[SoftwareTimingDAQ]] = {
-    BoardType.ARDUINO: Arduino_SoftTiming,
-    BoardType.LABJACK: LabJackU3_SoftTiming,
-    BoardType.NATIONAL_INSTRUMENTS: NI_SoftTiming
-}
+DAQ_CONSTRUCTORS: Dict[BoardType, Type[SoftwareTimingDAQ]] = {}
+
+try:
+    from .arduino import Arduino_SoftTiming
+    DAQ_CONSTRUCTORS.update({BoardType.ARDUINO: Arduino_SoftTiming})
+except:
+    print('Arduino not available, install pyfirmata')
+
+try:
+    from .labjack import LabJackU3_SoftTiming
+    DAQ_CONSTRUCTORS.update({BoardType.LABJACK: LabJackU3_SoftTiming})
+except:
+    print('Labjack not available, install exodriver')
+
+try:
+    from .national_instruments import NI_SoftTiming
+    DAQ_CONSTRUCTORS.update({BoardType.NATIONAL_INSTRUMENTS: NI_SoftTiming})
+except:
+    print('National Instruments not available, install nidaqmx')
+
+
